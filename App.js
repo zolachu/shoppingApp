5eca6cfb9f65f9db5c999950dc2f50193b536ed6
Zolboo Chuluunbaatar <zola@Zolboos-MBP.attlocal.net>
2020-10-08 02:32:57 -0700

    add

--- App.js
+++ App.js
@@ -1,36 +1,90 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { Component } from "react";
 
 import {
-  Dimensions,
   StyleSheet,
   Text,
-  TouchableHighlight,
   Image,
   SafeAreaView,
   View,
-  Alert,
-  Button,
   Platform,
+  ImageBackground,
+  FlatList,
 } from "react-native";
+
 import {
   useDimensions,
   useDeviceOrientation,
 } from "@react-native-community/hooks";
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Image source={require("./assets/background.png")} />
-    </SafeAreaView>
-  );
+import { Header } from "react-native/Libraries/NewAppScreen";
+
+const DATA = [
+  {
+    id: "1",
+    title: "First Item",
+  },
+  {
+    id: "2",
+    title: "Second Item",
+  },
+  {
+    id: "3",
+    title: "Third Item",
+  },
+];
+
+const Item = ({ title }) => (
+  <View>
+    <Text> {title}</Text>
+  </View>
+);
+
+export default class App extends Component {
+  render() {
+    const renderItem = ({ item }) => <Item title={item.title} />;
+
+    const [value, onChangeText] = React.useState("place holder");
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <ImageBackground
+          style={styles.logo}
+          source={require("./assets/background.png")}
+        >
+          <Header>
+            <Text>Items to buy</Text>
+          </Header>
+          <View style={styles.text}>
+            <FlatList
+              data={DATA}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id}
+            />
+            <TextInput
+              onChangeText={(text) => onChangeText(text)}
+              value={value}
+              Enter
+            />
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    width: "100%",
+    flexDirection: "column",
+  },
+  logo: {
     height: "100%",
+    width: "100%",
+    flex: 1,
+  },
+  text: {
+    textAlign: "center",
+    alignItems: "center",
+    fontFamily: "bold",
   },
 });
