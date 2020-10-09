15dcbac1fd33226aad8a273f6f8960783a009e41
zolachu <zolboo@stanford.edu>
2020-10-08 19:45:59 -0700

    weather

--- App.js
+++ App.js
@@ -1,64 +1,102 @@
 import { StatusBar } from "expo-status-bar";
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 import {
-  Dimensions,
   StyleSheet,
   Text,
   TouchableHighlight,
   Image,
-  SafeAreaView,
   View,
   Alert,
   Button,
-  Platform,
-  FlatList,
   ImageBackground,
   TextInput,
 } from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
 
-const DATA = [
-  { id: "1", title: "Zola" },
-  { id: "2", title: "Kevin" },
-  { id: "3", title: "Cookie" },
-];
-
-const Item = ({ title }) => (
-  <View>
-    <Text> {title} </Text>
-  </View>
-);
+const WEATHER_API_KEY = "e4226edb81f8dc101efa2256aac4af19";
+const BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
 export default function App() {
-  const renderItem = ({ item }) => <Item title={item.title} />;
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [unitsSystem, setUnitsSystem] = useState("metric");
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function load() {
+    try {
+      let { status } = await Location.requestPermissionsAsync();
+
+      if (status !== "granted") {
+        setErrorMessage("Access to location is needed to run the app");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+      // const latitude = location.latitude;
+      // const longitude = location.longitude;
+
+      const latitude = 37.0;
+      const longitude = -122.0;
+      //      const { latitude, longitude } = {location.coords};
+      console.log("latitude : " + latitude + ", longitude : " + longitude);
+
+      const weather_url =
+        "${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}";
+
+      const response = await fetch(weather_url);
+
+      const result = await response.json();
+
+      if (response.ok) {
+        setCurrentWeather(result);
+        console.log("current weather");
+        alert("currentWeather: " + currentWeather);
+      } else {
+        setErrorMessage(result.message);
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  }
+
+  // if (currentWeather) {
+  //   const {
+  //     main: { temp },
+  //   } = currentWeather;
   return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-        source={require("./assets/background.png")}
-      >
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
-        {/* <Button></Button> */}
-      </ImageBackground>
-    </SafeAreaView>
+    // {temp}
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.main}>
+        <Text>133</Text>
+      </View>
+    </View>
   );
 }
+//  else {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>{errorMessage}</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+// }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: "100%",
-    height: "100%",
+    justifyContent: "center",
+  },
+  main: {
+    justifyContent: "center",
+    flex: 1,
   },
 });
